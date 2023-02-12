4b87a1488fd8178db016f47cae82967ac6acf065
BrockMCarlson <brockmcarlson@gmail.com>
2023-02-12 14:52:04 -0600

    directorySetup commit before gitignore addition

--- directorySetup.m
+++ directorySetup.m
@@ -3,8 +3,8 @@ function [CODEDIR, IDXDIR, STIMDIR, OUTDIR] = directorySetup()
 %   Detailed explanation goes here
 
 global  CODEDIR IDXDIR STIMDIR OUTDIR
-    CODEDIR  = 'C:\Users\Brock\Documents\MATLAB\GitHub\BMC_AdaptdcosFigures\adaptdcos\Master figure releases\1.9 - iScience Submission';
-    IDXDIR   = 'C:\Users\Brock\Box\BRFS to iScience\FormattedDataOutputs\';
-    STIMDIR  = 'C:\Users\Brock\Documents\MATLAB\brfs_STIM_220827\';
-    OUTDIR   = 'C:\Users\Brock\Box\BRFS to iScience\plot outputs for BRFS to iScience\';
+    CODEDIR  = [];
+    IDXDIR   = [];
+    STIMDIR  = [];
+    OUTDIR   = [];
 end
